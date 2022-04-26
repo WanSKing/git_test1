d6ba8d8c9a68ca4300e9679243e6d24659edca65
John <John@qq.com>
2022-04-26 12:49:50 +0800

    first commit

---
+++ src/main/java/com/wsk/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.wsk.git;
+
+/**
+ * @author wsk
+ * @create 2022-04-26-12:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
