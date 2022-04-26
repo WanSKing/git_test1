55b267fe90b82b193fe810e8e66d940db446e0ef
John <John@qq.com>
2022-04-26 12:51:43 +0800

    second commit

--- src/main/java/com/wsk/git/GitTest.java
+++ src/main/java/com/wsk/git/GitTest.java
@@ -7,6 +7,7 @@ package com.wsk.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 
 }
